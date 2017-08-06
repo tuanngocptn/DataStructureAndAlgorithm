65b130095f6b793fb87fcda03d33e999943ab741
tuanngocptn <tuanngocptn@gmail.com>
2017-08-06 15:21:12 +0700

    [dsa] edit gitignore

--- Assignment1/dsaconsolemvn/src/main/java/Main.java
+++ Assignment1/dsaconsolemvn/src/main/java/Main.java
@@ -1,44 +1,37 @@
 import collection.LinkedLstDequeue;
-import collection.LinkedLstQueue;
-import entities.Order;
+import entities.Customer;
+import model.CustomerModel;
+import sort.SelectSort;
+import sort.Sort;
 
 public class Main {
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
-        LinkedLstDequeue<Order> linkStr = new LinkedLstDequeue<Order>();
-        Order order = new Order();
-        order.setCcode("Ccode");
-        order.setPcode("Pcode");
-        order.setQuantity(100);
+        LinkedLstDequeue<Customer> customerLinkedLstDequeue = new LinkedLstDequeue<Customer>();
+        for (int i = 3; i >= 0; i--) {
+            Customer customer = new Customer();
+            customer.setCcode("c"+i);
+            customer.setCusName("name");
+            customer.setPhone(Integer.toString(i));
+            customerLinkedLstDequeue.insertLast(customer);
+        }
+        CustomerModel.saveAll(customerLinkedLstDequeue);
 
-        linkStr.insertFirst(order);
-        linkStr.insertFirst(order);
-        linkStr.insertFirst(order);
-        linkStr.insertLast(order);
-        linkStr.insertLast(order);
-        linkStr.insertLast(order);
-        System.out.println(linkStr.length());
-        linkStr.delete(0);
-        System.out.println(linkStr.length());
-        linkStr.delete(0);
-        System.out.println(linkStr.length());
-        linkStr.delete(0);
-        System.out.println(linkStr.length());
-        linkStr.delete(0);
-        System.out.println(linkStr.length());
-        linkStr.delete(0);
-        System.out.println(linkStr.length());
-        linkStr.delete(0);
-        System.out.println(linkStr.length());
-        linkStr.insertAt(1,order);
-        System.out.println(linkStr.length());
-        linkStr.insertAt(1,order);
-        System.out.println(linkStr.length());
-        linkStr.insertAt(1,order);
-        System.out.println(linkStr.length());
-        linkStr.insertAt(1,order);
-        System.out.println(linkStr.length());
-        System.out.println(linkStr.displayForward());
+//        System.out.println(CustomerModel.getAll().displayForward());
 
-        linkStr.insertAt(1,order);
+       customerLinkedLstDequeue = CustomerModel.getAll();
+        Sort<Customer> sort = new SelectSort<Customer>() {
+            @Override
+            public boolean compare(Object o, Object o1) {
+                Customer customer = (Customer) o;
+                Customer customer1 = (Customer) o1;
+                return customer.getCcode().compareTo(customer1.getCcode()) > 0;
+            }
+        };
+        customerLinkedLstDequeue = sort.sort(customerLinkedLstDequeue);
+        System.out.println(customerLinkedLstDequeue.displayForward());
+
+
     }
+
 }
