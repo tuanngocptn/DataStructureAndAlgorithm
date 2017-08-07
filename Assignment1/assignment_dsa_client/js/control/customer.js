d589ac5d67412bddfbf7f06fe2800cf1f94a2feb
Pham Tuan Ngoc <ngoc.pt@java-ngocpt.sutrixmedia1.com>
2017-08-07 17:54:11 +0700

    [dsa-client] edit add button

--- Assignment1/assignment_dsa_client/js/control/customer.js
+++ Assignment1/assignment_dsa_client/js/control/customer.js
@@ -62,16 +62,18 @@ var customer = {
 
 var customerControl = {
 	loadToTblMain: function(data){
+		var action = "";
 		var tblMain = $("#tblMain");
 		tblMain.empty();
 		tblMain.append("<thead><tr><th>Customer Code</th><th>Customer Name</th><th>Customer Phone</th><th></th></tr></thead><tbody>");
 		if(typeof tblMain !== 'undefined' && data.length > 0){
 			for (var i = 0 ; i < data.length; i++) {
-				var action = 'customer.delete("'+data[i].ccode+'")';
+				action = 'customer.delete("'+data[i].ccode+'")';
 				tblMain.append("<tr><td>"+ data[i].ccode +"</td><td>"+ data[i].cusName +"</td><td>"+ data[i].phone +"</td><td onclick='"+ action +"'>Delete</td></tr>");
 			}
 		}
-		tblMain.append('</tbody><tfoot><tr><td><input type="text" name="ccode" id="ccode" required></td><td><input type="text" name="cusName" id="cusName" required></td><td><input type="text" name="phone" id="phone" required></td></tr></tfoot>');
+		action = "customer.add()"
+		tblMain.append('</tbody><tfoot><tr><td><input type="text" name="ccode" id="ccode" required></td><td><input type="text" name="cusName" id="cusName" required></td><td><input type="text" name="phone" id="phone" required></td><td onclick="'+ action +'">Add</td></tr></tfoot>');
 		
 	}
 }
